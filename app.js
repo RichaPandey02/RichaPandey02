82552bb996f79c902122f868df7efcd936660c14
Richa Pandey <pandeyricha0297@gmail.com>
2022-10-11 18:36:53 +0530

    analyzing the eshop database

--- app.js
+++ app.js
@@ -3,42 +3,23 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+app.use(cors());
+app.options('*', cors());
 require("dotenv/config");
+
+
+//Routes
+const productsRoutes = require('./routes/product');
 const api = process.env.API_URL;
+app.use(`\ ${api}/products`,productsRoutes);
 
-//model
-const productSchema = mongoose.Schema({
-    name: String,
-    image:String,
-    countInStock: Number
-})
-const Product = mongoose.model('Product',productSchema);
-
-app.get(`/${api}/products`, async(req, res) => {
-  const productList = await Product.find();
-  res.send(productList);
-});
-app.post(`/${api}/products`, (req,res)=>{
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct);
-    })).catch((err)=>{
-        res.status(500).json({
-            error:err,
-            success: false
-        })
-    })
-    res.send(product);
-})
 
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
---
+++ models/product.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const productSchema = mongoose.Schema({
+    name: String,
+    image:String,
+    countInStock: Number
+})
+const Product = mongoose.model('Product',productSchema);
