33999c80c9930d001c3ca3078cb5fdba14d51270
Richa Pandey <pandeyricha0297@gmail.com>
2022-10-11 18:10:54 +0530

    Install mongoose and connect to mongoDB Database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 
 //middleware
@@ -10,18 +11,31 @@ app.use(morgan('tiny'));
 require("dotenv/config");
 const api = process.env.API_URL;
 app.get(`/${api}/products`, (req, res) => {
-  const product = {
+  const productList = {
     id:1,
     name: "Hair dresser",
     image: "some URL"
   }
-  res.send(product);
+  res.send(productList);
 });
 app.post(`/${api}/products`, (req,res)=>{
     const newProduct = req.body;
     console.log(newProduct);
     res.send(newProduct);
 })
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
+
 app.listen(3000, () => {
     console.log(api);
   console.log("server is running http://localhost:3000");
