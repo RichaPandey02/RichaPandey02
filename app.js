00ffdba0feab972290d94cb1ce56b7e571e0d271
Richa Pandey <pandeyricha0297@gmail.com>
2022-10-11 17:28:39 +0530

    create first api call and parsing json data

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 require("dotenv/config");
 const api = process.env.API_URL;
-app.get(`${api}/products`, (req, res) => {
-  console.log("hello API!!!");
+app.get(`/${api}/products`, (req, res) => {
+  const product = {
+    id:1,
+    name: "Hair dresser",
+    image: "some URL"
+  }
+  res.send(product);
 });
+app.post(`/${api}/products`, (req,res)=>{
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
+})
 app.listen(3000, () => {
     console.log(api);
   console.log("server is running http://localhost:3000");
